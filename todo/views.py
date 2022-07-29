602cec5b2797d996daa4b826f5c316efeb8e9f15
Mike Nedviga <crocodilchi@gmail.com>
2022-07-29 16:07:35 +0300

    first commit

---
+++ todo/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout
+
+# Create your views here.
+def home(request):
+    return render(request, 'todo/home.html')
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {"form": UserCreationForm()})
+    else:
+        # Create a new user
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {"form": UserCreationForm(), 'error': 'That username has already been taken. Please choose another one, bitch.'})
+        else:
+            # Tell about the error
+            return render(request, 'todo/signupuser.html', {"form": UserCreationForm(), 'error': 'Password did not match, dumbass'})
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
