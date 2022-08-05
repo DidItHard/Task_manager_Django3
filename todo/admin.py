b52c574938e3277be0623a5c3e6048ab4248d842
Mike Nedviga <crocodilchi@gmail.com>
2022-08-05 16:33:25 +0300

    user login/logout

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ToDo
+
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('creation_date', )
 
 # Register your models here.
+admin.site.register(ToDo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=200, default='< no title >')
+    decription = models.TextField(blank=True)
+    creation_date = models.DateField(auto_now_add=True)
+    # importance = (
+    #     ('High', 'It\'s important'),
+    #     ('Common', 'Common task'),
+    #     ('Low', 'Just trifle')
+    # )
+    # importance = models.CharField(max_length=100, choices=importance)
+    important = models.BooleanField(default=False)
+    deadline = models.DateField(null=True, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+
+    def __str__(self):
+        return self.title
